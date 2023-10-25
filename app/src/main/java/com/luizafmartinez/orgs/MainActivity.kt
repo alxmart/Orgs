c166da34c43276668f71c1d06f26801ac9751cc1
alxmart <alxmartinez@gmail.com>
2023-10-25 12:00:48 -0300

    Added fun onCreate and toast for testing.

--- app/src/main/java/com/luizafmartinez/orgs/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/orgs/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.luizafmartinez.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+       Toast.makeText(this,"Bem vindo(a) ao Orgs!",Toast.LENGTH_SHORT).show()
+    }
+
+
+
+
+
 }
